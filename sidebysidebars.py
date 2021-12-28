f35d96ff07c33771ecec740264a3573cc2a18fad
Marina Vianna <87891644+marinavianna@users.noreply.github.com>
2021-12-28 17:11:14 +0000

    Add graph

--- sidebysidebars.py
+++ sidebysidebars.py
@@ -13,3 +13,22 @@ def create_x(t, w, n, d):
     # t = number of datasets
     # w = width of each bar
     # d = number of sets of bars (columns)
+
+# graphs
+
+school_a_x = create_x(2,0.8,1,5)
+school_b_x = create_x(2,0.8,2,5)
+plt.figure(figsize=(10,8))
+ax=plt.subplot()
+plt.bar(school_a_x, middle_school_a, color='dodgerblue')
+plt.bar(school_b_x,middle_school_b, color='mediumaquamarine')
+middle_x = [(a+b)/2 for a,b in zip(school_a_x,school_b_x)]
+ax.set_xticks(middle_x)
+ax.set_xticklabels(unit_topics)
+ax.axis([0,10,70,90])
+plt.legend(['Middle School A', 'Middle School B'])
+plt.title('Test Averages on Different Units')
+plt.xlabel('Unit')
+plt.ylabel('Test Average')
+plt.savefig('my_side_by_side.png')
+plt.show()

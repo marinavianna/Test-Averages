a6484e0936f47a9e3476ff35eb0653580be377e9
Marina Vianna <87891644+marinavianna@users.noreply.github.com>
2021-12-28 17:04:16 +0000

    Add data

---
+++ sidebysidebars.py
@@ -0,0 +1,7 @@
+from matplotlib import pyplot as plt
+
+# data
+unit_topics = ['Limits', 'Derivatives', 'Integrals', 'Diff Eq', 'Applications']
+middle_school_a = [80, 85, 84, 83, 86]
+middle_school_b = [73, 78, 77, 82, 86]
+
